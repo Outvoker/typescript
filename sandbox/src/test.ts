197835d3d6140285030866f52577e73630822574
Outvoker <704457936@qq.com>
2019-12-09 11:22:36 +0800

    test

--- sandbox/src/test.ts
+++ sandbox/src/test.ts
@@ -1,8 +1,4 @@
-function buildName(firstName:string) {
-    var restOfName = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        restOfName[_i - 1] = arguments[_i];
-    }
-    return firstName + " " + restOfName.join(" ");
-}
-var buildNameFun = buildName;
+
+const arr = [1,2,3]
+arr.map(x=>x+100)
+console.log(arr)
